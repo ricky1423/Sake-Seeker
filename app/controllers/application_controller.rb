266cadae5404934580f234cd332b2d9a99eeb2f4
ricky1423 <rikisoccer0904@gmail.com>
2024-05-29 14:09:37 +0900

    ホーム画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["ORI_APP_USER"] && password == ENV["ORI_APP_PASSWORD"]
+    end
+  end
 end
