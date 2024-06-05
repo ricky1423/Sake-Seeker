ef91e7445a0fcf91f3df9c300fa53310835063f7
ricky1423 <rikisoccer0904@gmail.com>
2024-06-06 00:19:24 +0900

    非同期設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,14 @@
 class LikesController < ApplicationController
 
-  before_action :set_item
+  before_action :set_item, only: [:create, :destroy]
 
   def create
     like = current_user.likes.build(item_id: params[:item_id])
     like.save
     respond_to do |format|
-      format.js
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update("favorite", partial: 'likes/like', locals: { item: @item })
+      end
     end
   end
 
@@ -14,7 +16,9 @@ class LikesController < ApplicationController
     like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
     like.destroy
     respond_to do |format|
-      format.js
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update("favorite", partial: 'likes/like', locals: { item: @item })
+      end
     end
   end
 
